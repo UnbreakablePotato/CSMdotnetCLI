46f2272e1c0d03eca4e6f0fbd6a99eba5a9688ca
UnbreakablePotato <swipeytwit@gmail.com>
2026-09-01 21:29:38 +0200

    Repl to clean CLI input

---
+++ CSMdotnet/Repl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSMdotnet
+{
+    internal class Repl
+    {
+        public static string[] cleanInput(string input)
+        {
+            string[] res;
+            res = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            return res;
+        }
+    }
+}
