f8734ae2195ec29ac84e16e6af3b19a699583e8e
UnbreakablePotato <swipeytwit@gmail.com>
2026-09-02 16:31:27 +0200

    Expanded commands to support the ladder command

--- CSMdotnet/Program.cs
+++ CSMdotnet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CSMdotnet;
+using DotNetEnv;
 using System;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -6,11 +7,12 @@ using System.Threading.Tasks;
 
 public class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
+        Env.Load();
         while (true)
         {
-            string userInput;
+            string? userInput;
 
             Console.Write("$ ");
             try
@@ -31,20 +33,42 @@ public class Program
             {
                 Environment.Exit(1);
             }
+            if (commandInput[0] == "search")
+            {
+                string? gameName = commandInput[1];
+                string? tagLine = commandInput[2];
+
+                await Commands.Search(gameName, tagLine);
+            }
+            if (commandInput[0] == "ladder")
+            {
+                string? region = commandInput[1];
+
+                await Commands.Ladder(region);
+            }
 
         }
     }
 }
 
-public record GetPuuid(string Puuid,string GameName, string TagLine);
+/*public record GetPuuid(string? Puuid,string? GameName, string? TagLine);
 
 public class PuuidService
 {
+
+    private string gameName;
+    private string tagLine;
+
+    public PuuidService(string GameName, string TagLine)
+    {
+        gameName = GameName;
+        tagLine = TagLine;
+    }
     private static readonly HttpClient _httpClient = new HttpClient();
 
-    public async Task<GetPuuid?> GetPuuidAsync(int id)
+    public async Task<GetPuuid?> GetPuuidAsync()
     {
-        string fullUrl = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/SUPER%20CARRY/GANK?api_key=";
+        string fullUrl = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + gameName + "/" + tagLine + "?api_key=";
 
         try
         {
@@ -57,4 +81,4 @@ public class PuuidService
             return null;
         }
     }
-}
+}*/
